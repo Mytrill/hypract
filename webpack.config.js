9928ec822d6a6c0c345eba1f6a140254049378be
Anthony Ferrando <anthony.ferrando@Braintribe>
2017-08-23 06:44:43 +0200

    Update build process.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,48 @@ const isProduction = process.argv.indexOf('-p') >= 0;
 const sourcePath = path.join(__dirname, './src');
 const outPath = path.join(__dirname, './dist');
 
+
 module.exports = {
+  entry: {
+    'hypract': './src/index.ts',
+    'hypract.min': './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, outPath),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'hypract',
+    umdNamedDefine: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+      include: /\.min\.js$/,
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+      exclude: /node_modules/,
+      query: {
+        declaration: false,
+      }
+    }]
+  }
+}
+
+
+const ignored = {
   context: __dirname,
   entry: {
     main: './src/index.ts',
