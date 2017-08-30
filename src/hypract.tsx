18185cb955d6a32f4ca21cbe65a7f225ad81d72c
Anthony Ferrando <anthony.ferrando@Braintribe>
2017-08-30 19:57:20 +0200

    Set the type for hypract()'s initial state

--- src/hypract.tsx
+++ src/hypract.tsx
@@ -1,25 +1,23 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import { applyMiddleware, createStore, Reducer, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
+import * as React from 'react'
+import { render } from 'react-dom'
+import { applyMiddleware, createStore, Reducer, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
-import { ReactElement } from './types';
-import { thunk, logger } from './middlewares';
-import { State, hypractReducer } from './reducer';
+import { ReactElement } from './types'
+import { thunk, logger } from './middlewares'
+import { State, hypractReducer, HypractState } from './reducer'
 
 export interface HypractConf {
-  appReducer?: Reducer<any>;
-  useLogger?: boolean;
-  initialState?: any;
+  appReducer?: Reducer<any>
+  useLogger?: boolean
+  initialState?: HypractState
 }
 
 export const hypract = (App: ReactElement, { appReducer, useLogger, initialState = {} }: HypractConf = {}) => {
-  const reducer = appReducer ? combineReducers<State>({ app: appReducer, hypract: hypractReducer(initialState) }) : combineReducers<State>({ hypract: hypractReducer(initialState) })
-  
-  const store = createStore(reducer, useLogger ? applyMiddleware(thunk, logger) : applyMiddleware(thunk));
-  render(
-    <Provider store={store}>
-      {App}
-    </Provider>
-  ,document.body);
+  const reducer = appReducer
+    ? combineReducers<State>({ app: appReducer, hypract: hypractReducer(initialState) })
+    : combineReducers<State>({ hypract: hypractReducer(initialState) })
+
+  const store = createStore(reducer, useLogger ? applyMiddleware(thunk, logger) : applyMiddleware(thunk))
+  render(<Provider store={store}>{App}</Provider>, document.body)
 }
