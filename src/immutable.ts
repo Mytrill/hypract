544cbd07e0bf4003f681399e80204fbc583dc9c3
Anthony Ferrando <anthony.ferrando@Braintribe>
2017-08-30 20:27:57 +0200

    Add merge() to immutable

--- src/immutable.ts
+++ src/immutable.ts
@@ -1,4 +1,4 @@
-import { get as dotGet, set as dotSet } from './immutable'
+import { get as dotGet, merge as dotMerge, set as dotSet } from 'dot-prop-immutable'
 
 /**
  * Get the nested value in the given object. Return undefined if does not exists.
@@ -9,6 +9,21 @@ import { get as dotGet, set as dotSet } from './immutable'
  */
 export const get = (object: any, path: number | string | Array<number | string>): any => {
   return dotGet(object, path)
+}
+
+/**
+ * Merges a value.  The target value must be an object, array, null, or undefined.
+ * If target is an object, Object.assign({}, target, param) is used.
+ * If target an array, target.concat(param) is used.
+ * If target is null or undefined, the value is simply set.
+ * 
+ * @param object The object to evaluate.
+ * @param path The path to the value.
+ * @param val The value to merge into the target value.
+ * @return the newly created object
+ */
+export const merge = (object: any, path: number | string | Array<number | string>, val: any): any => {
+  return dotMerge(object, path, val)
 }
 
 /**
@@ -28,7 +43,7 @@ export const push = <T>(array: T[], ...val: T[]): T[] => {
  * @param object the object to set the value into (not mutated)
  * @param path the path to set the value to, e.g. 'child.name'
  * @param value the value to set
- * @return the newly generated object
+ * @return the newly created object
  */
 export const set = (object: any, path: number | string | Array<number | string>, value: any): any => {
   return dotSet(object, path, value)
