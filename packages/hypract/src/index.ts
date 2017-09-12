300128be4e5a8170749668d4a1f3f89df5806d25
Anthony Ferrando <ferrando.tony@gmail.com>
2017-09-12 11:56:49 +0200

    re-export react, react-redux and redux

--- packages/hypract/src/index.ts
+++ packages/hypract/src/index.ts
@@ -16,6 +16,16 @@ import * as immutable from './immutable'
 export { immutable }
 
 export * from './middlewares'
+
+import React from 'react'
+export { React }
+
+import * as redux from 'redux'
+export { redux }
+
+import * as reactRedux from 'react-redux'
+export { reactRedux }
+
 export * from './rootReducer'
 export * from './types'
 export * from './utils'
